2f4d6a85342b4e166ebcb20e0f92b8531ac09dd3
Revan Sopher <rsopher@gmail.com>
2022-11-27 16:09:10 -0500

    fixes, install ghostscript, font

--- .github/workflows/test_image.sh
+++ .github/workflows/test_image.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# Creates unique image containing current branch name.
-convert -background darkgreen -fill purple -pointsize 72 label:"$(git branch --show-current)" /tmp/branch.png
+convert -font FreeMono -background darkgreen -fill purple -pointsize 72 label:"$(git rev-parse --short HEAD)" /tmp/branch.png
